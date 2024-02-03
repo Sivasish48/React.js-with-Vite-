0c80584422bcb9e584cadbc6f06024507ed589eb
Sivasish Praharaj <sivasish48@gmail.com>
2024-02-03 20:28:59 +0530

    bgColorChange with useState()

---
+++ bgChanger06/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color,setColor] = useState("black")
+ 
+  
+  return (
+ 
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+          <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+          
+          <button
+          onClick={() => setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+          <button
+          onClick={() => setColor("pink")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "pink"}}
+          >Pink</button>
+          <button
+          onClick={() => setColor("lavender")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "lavender"}}
+          >Lavender</button>
+         
+        </div>
+      </div>
+    </div>
+       )
+}
+
+export default App
