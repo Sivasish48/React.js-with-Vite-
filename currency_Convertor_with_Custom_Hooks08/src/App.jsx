58748a713ca0214da11d6a5f308994b6a2550ec9
Sivasish Praharaj <sivasish48@gmail.com>
2024-02-11 23:10:47 +0530

    custom hooks

---
+++ currency_Convertor_with_Custom_Hooks08/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-pink bg-black'>hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ currency_Convertor_with_Custom_Hooks08/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,24 @@
+// Custom Hooks 
+// Custom hooks in React are reusable JavaScript functions that contain logic related to state management, side effects, or any other functionality that a predefined hooks have.
+
+// example of a custom hooks
+
+function hello (){
+    return[]
+}
+
+// We can also integrate the predefined hooks with custom hooks
+
+import {useEffect,useState} from "react"
+
+// lets use a function
+
+function useCurrencyInfo(currency){
+    // I want to call an api and now here I want to immplement this hook when someone will call it.
+
+    useEffect(()=>{
+        
+    })
+}
+
+
