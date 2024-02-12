9b702a222ba87adc50d642e7f7101a9c1479bd5d
Sivasish Praharaj <sivasish48@gmail.com>
2024-02-12 21:47:05 +0530

    custom hooks with inputbox component

--- currency_Convertor_with_Custom_Hooks08/src/hooks/useCurrencyInfo.js
+++ currency_Convertor_with_Custom_Hooks08/src/hooks/useCurrencyInfo.js
@@ -15,10 +15,20 @@ import {useEffect,useState} from "react"
 
 function useCurrencyInfo(currency){
     // I want to call an api and now here I want to immplement this hook when someone will call it.
-
+   
+    const [data,setData] = useState({})
     useEffect(()=>{
-        
-    })
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)  // Api is called using fetch in a use Effect hoo and we can use chaining as well.
+        .then((response)=>{
+            JSON.parse(response)
+        })
+        .then((json_response)=>{
+            // now to update the json data we need to use the useState hook.
+            return setData(json_response[currency])
+        })
+    },[currency])
+
+    return data;
 }
 
-
+export default useCurrencyInfo;
