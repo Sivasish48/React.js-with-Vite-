bf85d6c775be116ca008ceec3866ced3c721a9b6
Sivasish Praharaj <sivasish48@gmail.com>
2024-02-13 10:58:47 +0530

    useId() hook

--- currency_Convertor_with_Custom_Hooks08/src/App.jsx
+++ currency_Convertor_with_Custom_Hooks08/src/App.jsx
@@ -1,10 +1,29 @@
 import { useState } from 'react'
-
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
   
+   // here make an useState hook
 
+   const [amount,setAmount] = useState(0)  // This hook is used to set the amount 
+   const [from,setFrom] = useState("inr")   // This hook sets which currency type is going to be converted
+   const [to,setTo] = useState("usd")        // This hook sets in which currency  the given currency type will be changed.
+   const [convertedAmount,setConvertedAmount] = useState(0)  // This hook is for the converted amount
+
+   const currencyInfo = useCurrencyInfo(from) 
+
+   const optns = Object.keys(currencyInfo)
+
+   // now for swapping feature where currency type changes as well as the amount and converted amount also changes.
+     
+   const swap = ()=>{
+    setFrom(to)
+    setTo(from)
+    convertedAmount(amount)
+    setAmount(convertedAmount) 
+   }
   return (
     <>
       <h1 className='text-pink bg-black'>hello</h1>
--- currency_Convertor_with_Custom_Hooks08/src/components/InputBox.jsx
+++ currency_Convertor_with_Custom_Hooks08/src/components/InputBox.jsx
@@ -1,51 +1,67 @@
-import React from "react";
+import React, { useId } from "react";
 
+// now creating an input box
 function InputBox({
-    label,
-    amount,
-    onAmountChange,
-    onCurrencyChange,
-    currencyOptions = [],
-    selectCurrency = "usd",
-    amountDisable = false,
-    currencyDisable = false,
-    className = "",
+    label,    // label to show to and from
+    amount,   // amount that needs to be converted into an differnct currency's amount.
+    onAmountChange,  // function when amounts get changed
+    onCurrencyChange,  // function for currency changing
+    currencyOptions = [],  // currency option to be put in the selectCurrency
+    selectCurrency="inr",  // type of currency
+    amountDisable =false,  // the feature for disabling the input to take ammount is "false"
+    currencyDisable = false, //  the feature for disabling the input to take type of currency is "false"
+    className = "",  // any custom classes that an user wants to put
 }) {
-   const amountInputId = useId()
+   
+   // Now after all , use useId() hook 
+   // useId() hook is used to generate unique ids for related components.
+   // note that it shouldnot be used for the generation for keys
 
+   const inputeId = useId()
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
             <div className="w-1/2">
-                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
-                    {label}
+                <label htmlFor={inputeId} className="text-black/40 mb-2 inline-block">
+                    {label}  // label like from or to
                 </label>
                 <input
-                    id={amountInputId}
+                    
+                    id={inputeId}
                     className="outline-none w-full bg-transparent py-1.5"
-                    type="number"
+                    type="number" // the data would be a number
                     placeholder="Amount"
-                    disabled={amountDisable}
-                    value={amount}
-                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                    disabled={amountDisable}  // The feature to not disable (false)
+                    value={amount}     // amount
+                    onChange={(e) =>{
+                        // returning both the function(existance) and execution of the function(  for extra optimization.)
+                        return onAmountChange && onAmountChange(Number(e.target.value)) // the value wil be converted into a number.
+                    }}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    value={selectCurrency}
-                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-                    disabled={currencyDisable}
+                    value={selectCurrency}  // selecting currency type from the options
+                    onChange={(e)=>{
+                         // returning both the function(existance) and execution of the function(  for extra optimization.)
+                        return onCurrencyChange && onCurrencyChange(e.target.value) // the currency type will be a string.
+                    }}  
+                    disabled ={currencyDisable}// feature to not disabling the curreny(false)
                 >
-                    
-                        {currencyOptions.map((currency) => (
-                            <option key={currency} value={currency}>
-                            {currency}
-                            </option>
-                        ))}
+                      
+                      {/* to loop through the options to select currency type using the map()*/}
+                       {currencyOptions.map((c)=>{
+                           // key for being more particular about the selected currency type.
+                           <option key={c} value={c}>
+                            {c}
+                           </option> 
+                       })}
                 
                 </select>
             </div>
         </div>
     );
 }
+
+export default InputBox;
