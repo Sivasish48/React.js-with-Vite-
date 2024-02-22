4f3ba91642cbdfa6302d92831ff2ad853b53300b
Sivasish Praharaj <98220976+Sivasish48@users.noreply.github.com>
2024-02-22 10:19:09 +0530

    Update User.jsx

--- reactRouter09/src/Components/User/User.jsx
+++ reactRouter09/src/Components/User/User.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom' 
 
-
+// Using params in React allows you to pass dynamic parameters to a component through the URL. 
 function User(){
     const {userId} = useParams()
 
