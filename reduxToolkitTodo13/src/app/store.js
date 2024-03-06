e947b180f597a1527b1762ecc60aca0638957ac7
Sivasish Praharaj <sivasish48@gmail.com>
2024-03-06 23:22:26 +0530

    reduxtoolkit with slices reducers stores with integrationg multiple functionality

---
+++ reduxToolkitTodo13/src/app/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+// now import the reducers 
+import todoReducer from '../features/todo/todoSlice'
+// After installing React Redux, you need to configure a Redux store to manage the state of your React application.
+// That u=is why we need to create a store file in the react app
+
+//  The configureStore function is part of Redux Toolkit, which is the recommended way to set up a Redux store.
+
+// Redux Toolkit aims to simplify Redux development by reducing boilerplate code.
+// configureStore({}) sets up a Redux store with default middleware, including thunk middleware for handling asynchronous actions.
+
+// now add the key values here
+
+const store = configureStore({
+    reducer:todoReducer
+})
